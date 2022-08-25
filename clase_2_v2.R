43c9a89cd6d2c489a82308afdec5e6029ecfb364
josermirandah <jose.r.miranda.h@gmail.com>
2022-08-25 08:43:50 +0000

    Se agrego el RMD para explorar plantas extintas

--- clase_2_v2.R
+++ clase_2_v2.R
@@ -4,3 +4,9 @@ install.packages("hms")
 library(tidyverse)
 library(knitr)
 library(kableExtra)
+
+
+
+plants <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/plants.csv')
+actions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/actions.csv')
+threats <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/threats.csv')

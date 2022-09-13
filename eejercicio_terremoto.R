202229acd55b86d0c83b7f6f0e8de9dc24236370
josermirandah <joser.miranda.h@gmail.com>
2022-09-13 07:10:02 +0000

    segunda carga nprueba

--- eejercicio_terremoto.R
+++ eejercicio_terremoto.R
@@ -8,3 +8,10 @@ author: "Derek Corcoran"
 date: "`r format(Sys.time(), '%d/%m, %Y')`"
 output: html_document
 ---
+  
+  
+  ```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = FALSE)
+knitr::opts_chunk$set(message = FALSE)
+knitr::opts_chunk$set(warning=FALSE)
+```
